24a18bdc94dced4ea5f11e7fece35d591745edbc
Orion / Emilien <contact@oriondev.fr>
2025-07-23 04:33:53 +0200

    add help command

--- colors/models.go
+++ colors/models.go
@@ -3,22 +3,23 @@ package colors
 type Color uint
 
 const (
-	Lesbian     Color = 1 << iota // Done
-	Gay                           // Done
-	Bisexual                      // Done
-	Transgender                   // Done
-	Pansexual                     // Done
-	Asexual                       // Done
-	NonBinary                     // Done
-	Aromantic                     // Done
-	Polysexual                    // Done
-	DemiBoy                       // Done
-	DemiGirl                      // Done
-	Agender                       // Done
-	Bigender                      // Done
-	Genderfluid                   // Done
-	Neutrois                      // Done
-	Trigender                     // Done
-	Femboy                        // Done
-	MenLovingMen
+	Lesbian      Color = 1 << iota // Done
+	Gay                            // Done
+	Bisexual                       // Done
+	Transgender                    // Done
+	Pansexual                      // Done
+	Asexual                        // Done
+	NonBinary                      // Done
+	Aromantic                      // Done
+	Polysexual                     // Done
+	DemiBoy                        // Done
+	DemiGirl                       // Done
+	Agender                        // Done
+	Bigender                       // Done
+	Genderfluid                    // Done
+	Neutrois                       // Done
+	Trigender                      // Done
+	Femboy                         // Done
+	MenLovingMen                   // Done
+	Undefined                      // Done
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 
 	"github.com/oriionn/queer/colors"
@@ -17,28 +18,31 @@ type Argument struct {
 }
 
 var args []Argument = []Argument{
-	{[]string{"agender", "ag"}, colors.Agender, "Print the agender flag"},
-	{[]string{"aromantic", "ar"}, colors.Aromantic, "Print the aromantic flag"},
-	{[]string{"asexual", "as"}, colors.Asexual, "Print the asexual flag"},
-	{[]string{"bigender", "big"}, colors.Bigender, "Print the bigender flag"},
-	{[]string{"bisexual", "bi", "b"}, colors.Bisexual, "Print the bisexual flag"},
-	{[]string{"demiboy", "db"}, colors.DemiBoy, "Print the demiboy flag"},
-	{[]string{"demigirl", "dg"}, colors.DemiGirl, "Print the demigirl flag"},
-	{[]string{"femboy", "f"}, colors.Femboy, "Print the femboy flag"},
-	{[]string{"gay", "g"}, colors.Gay, "Print the gay flag"},
-	{[]string{"genderfluid", "ge"}, colors.Genderfluid, "Print the genderfluid flag"},
-	{[]string{"lesbian", "l"}, colors.Lesbian, "Print the lesbian flag"},
-	{[]string{"menlovingmen", "mlm"}, colors.MenLovingMen, "Print the men loving men flag"},
-	{[]string{"neutrois", "ne"}, colors.Neutrois, "Print the neutrois flag"},
-	{[]string{"nonbinary", "nb"}, colors.NonBinary, "Print the non binary flag"},
-	{[]string{"pansexual", "pan", "pa"}, colors.Pansexual, "Print the pansexual flag"},
-	{[]string{"polysexual", "poly", "po"}, colors.Polysexual, "Print the polysexual flag"},
-	{[]string{"transgender", "trans", "t"}, colors.Transgender, "Print the transgender flag"},
-	{[]string{"trigender", "tr"}, colors.Trigender, "Print the trigender flag"},
+	{[]string{"agender", "ag"}, colors.Agender, "print the agender flag"},
+	{[]string{"aromantic", "ar"}, colors.Aromantic, "print the aromantic flag"},
+	{[]string{"asexual", "as"}, colors.Asexual, "print the asexual flag"},
+	{[]string{"bigender", "big"}, colors.Bigender, "print the bigender flag"},
+	{[]string{"bisexual", "bi", "b"}, colors.Bisexual, "print the bisexual flag"},
+	{[]string{"demiboy", "db"}, colors.DemiBoy, "print the demiboy flag"},
+	{[]string{"demigirl", "dg"}, colors.DemiGirl, "print the demigirl flag"},
+	{[]string{"femboy", "f"}, colors.Femboy, "print the femboy flag"},
+	{[]string{"gay", "g"}, colors.Gay, "print the gay flag"},
+	{[]string{"genderfluid", "ge"}, colors.Genderfluid, "print the genderfluid flag"},
+	{[]string{"lesbian", "l"}, colors.Lesbian, "print the lesbian flag"},
+	{[]string{"menlovingmen", "mlm"}, colors.MenLovingMen, "print the men loving men flag"},
+	{[]string{"neutrois", "ne"}, colors.Neutrois, "print the neutrois flag"},
+	{[]string{"nonbinary", "nb"}, colors.NonBinary, "print the non binary flag"},
+	{[]string{"pansexual", "pan", "pa"}, colors.Pansexual, "print the pansexual flag"},
+	{[]string{"polysexual", "poly", "po"}, colors.Polysexual, "print the polysexual flag"},
+	{[]string{"transgender", "trans", "t"}, colors.Transgender, "print the transgender flag"},
+	{[]string{"trigender", "tr"}, colors.Trigender, "print the trigender flag"},
 }
 
 func main() {
-	var color colors.Color
+	var (
+		color colors.Color = colors.Undefined
+		help  bool
+	)
 
 	for _, arg := range args {
 		for _, name := range arg.args {
@@ -49,7 +53,20 @@ func main() {
 		}
 	}
 
+	flag.BoolVar(&help, "help", false, "")
+	flag.BoolVar(&help, "h", false, "")
+
 	flag.Parse()
+
+	if color == colors.Undefined {
+		index := rand.Intn(len(args))
+		color = args[index].value
+	}
+
+	if help == true {
+		printHelp()
+		return
+	}
 
 	width, height, err := term.GetSize(int(os.Stdout.Fd()))
 	if err != nil {
@@ -58,4 +75,30 @@ func main() {
 	}
 
 	utils.PrintFlag(width, height, colors.Get(color))
+}
+
+func printHelp() {
+	fmt.Println("")
+	fmt.Println("Usage:")
+	fmt.Println("  queer [options]")
+	fmt.Println("")
+	fmt.Println("Options:")
+	fmt.Println("  -h, --help                     show this help menu")
+	fmt.Println("")
+	for _, arg := range args {
+		fmt.Printf("")
+
+		argsList := ""
+		for i, name := range arg.args {
+			if i == 0 {
+				argsList += fmt.Sprintf("--%s", name)
+			} else {
+				argsList += fmt.Sprintf(", -%s", name)
+			}
+		}
+		fmt.Printf("%s", argsList)
+
+		fmt.Printf("%s %s\n", utils.Repeat(" ", 30-len(argsList)), arg.description)
+	}
+	fmt.Println("")
 }
